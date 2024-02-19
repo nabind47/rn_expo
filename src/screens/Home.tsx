504c9a6d84e99c011f57efea5ef4e667f878a371
nabind47 <nabindhami619@gmail.com>
2024-02-19 22:26:24 +0545

    feat: with spring and timing

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,32 +1,77 @@
-import { Button, StyleSheet, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
 
-import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
+const duration = 2000;
 
 const Home = () => {
-  const width = useSharedValue(100);
+  const defaultAnim = useSharedValue(200);
+  const linear = useSharedValue(200);
 
-  const handlePress = () => {
-    width.value = withSpring(width.value + 50);
-  };
+  const animatedDefault = useAnimatedStyle(() => ({
+    transform: [{ translateX: defaultAnim.value }],
+  }));
+  const animatedChanged = useAnimatedStyle(() => ({
+    transform: [{ translateX: linear.value }],
+  }));
+
+  React.useEffect(() => {
+    linear.value = withRepeat(
+      withTiming(-linear.value, {
+        duration,
+        easing: Easing.linear,
+      }),
+      -1,
+      true
+    );
+    defaultAnim.value = withRepeat(
+      withTiming(-defaultAnim.value, {
+        duration,
+      }),
+      -1,
+      true
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Animated.View
-        style={{
-          width,
-          height: 100,
-          backgroundColor: "violet",
-        }}
-      />
-      <Button onPress={handlePress} title="Click me" />
+      <Animated.View style={[styles.box, animatedDefault]}>
+        <Text style={styles.text}>in out</Text>
+      </Animated.View>
+      <Animated.View style={[styles.box, animatedChanged]}>
+        <Text style={styles.text}>linear</Text>
+      </Animated.View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    justifyContent: "center",
+    flex: 1,
     alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+  },
+  box: {
+    height: 80,
+    width: 80,
+    margin: 20,
+    borderWidth: 1,
+    borderColor: "#b58df1",
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#b58df1",
+    textTransform: "uppercase",
+    fontWeight: "bold",
   },
 });
 
